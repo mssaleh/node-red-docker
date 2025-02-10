3d74c20c8baf89449386f5531d83f622ce177963
mssaleh <mssaleh@live.com>
2025-02-10 23:33:02 +0400

    release

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Ensure /data is writable, then place a default settings.js if missing
-if [ ! -f "/data/settings.js" ]; then
-  cp /usr/src/node-red/config/settings.js /data/settings.js
-fi
-
-exec npm start -- --userDir /data
